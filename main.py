b194c0f12dc8c5d3eb1b300659ce58720a635582
Rupasri <prupasri3102@gmail.com>
2024-06-23 12:44:07 +0530

    my commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+
+def convert_curr():
+    init_currency = input("Enter the initial currency: ")
+    target_currency = input("Enter the target currency: ")
+
+    while True:
+        try:
+            amount = float(input("Enter the amount: "))
+        except:
+            print("The amounts need to be numeric...")
+            continue
+
+        if not amount > 0:
+            print("Amount needs to be greater than zero... ")
+
+        else:
+            break
+
+    url = f"https://api.apilayer.com/fixer/convert?to={target_currency}&from={init_currency}&amount={amount}"
+
+    payload = {}
+    headers= {
+      "apikey": "RbGLHRyAoM6sSLRRgVBYIL6lO6uCB1KK"
+    }
+    response = requests.request("GET", url, headers=headers, data = payload)
+    status_code = response.status_code
+
+    if status_code != 200:
+        result = response.json()
+        print("Error response: " + str(result))
+        quit()
+    
+    result = response.json()
+    print("Conversion Result: " + str(result))
+
+if __name__ == '__main__':
+    convert_curr()
